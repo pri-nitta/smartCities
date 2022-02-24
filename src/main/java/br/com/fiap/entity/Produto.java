614c362b3d3ed897cde133865ec80e479b892e60
Priscila Nitta <priscila.nitta@gmail.com>
2022-02-23 23:58:57 -0300

    adding columns

--- src/main/java/br/com/fiap/entity/Produto.java
+++ src/main/java/br/com/fiap/entity/Produto.java
@@ -1,8 +1,33 @@
 package br.com.fiap.entity;
 
+import java.util.Calendar;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tb_produto")
 public class Produto {
+	
+	@Id
+	@SequenceGenerator(name ="produto", sequenceName="sq_tb_produto", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="produto")
+	@Column(name="cd_produto")
+	private int id;
+	
+	@Column(name="nm_produto", nullable=false, length=100)
+	private String nome;
+	
+	@Column(name="vl_preco")
+	private double preco;
+	
+	@Column(name="dt_validade")
+	private Calendar dataValidade;
+	
 
 }
