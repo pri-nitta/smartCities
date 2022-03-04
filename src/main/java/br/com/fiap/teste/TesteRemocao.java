cb09a4b60cf97d1d9bccf720ef56c847442e3d56
Priscila Nitta <priscila.nitta@gmail.com>
2022-03-04 00:06:46 -0300

    remove and merge

--- src/main/java/br/com/fiap/teste/TesteRemocao.java
+++ src/main/java/br/com/fiap/teste/TesteRemocao.java
@@ -3,31 +3,28 @@ package br.com.fiap.teste;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import br.com.fiap.entity.Cliente;
 
-public class TesteBusca {
+public class TesteRemocao {
 
 	public static void main(String[] args) {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("smartcities");
 		EntityManager em = fabrica.createEntityManager();
-
-//		Busca
-		Cliente cliente = em.find(Cliente.class, 1);
 		
-		//desanexado, substitui
-//		Cliente cliente = new Cliente(1, "Thiago");
-			
-		System.out.println(cliente.getId() + " " + cliente.getNome());
+		Cliente cliente = em.find(Cliente.class, 1);
+		em.remove(cliente);
 
-		em.merge(cliente);
-
-		// atualização
-//		cliente.setNome("Henrique");
-
+		
+		try {
 		em.getTransaction().begin();
 		em.getTransaction().commit();
-
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		
 		em.close();
 		fabrica.close();
 
