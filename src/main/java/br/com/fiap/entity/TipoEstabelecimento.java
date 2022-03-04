f6e5b3b0ddba6b0adc5a231b54be6254d2c2e73c
Priscila Nitta <priscila.nitta@gmail.com>
2022-03-04 16:10:20 -0300

    many to one

--- src/main/java/br/com/fiap/entity/TipoEstabelecimento.java
+++ src/main/java/br/com/fiap/entity/TipoEstabelecimento.java
@@ -1,10 +1,13 @@
 package br.com.fiap.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -20,5 +23,45 @@ public class TipoEstabelecimento {
 	
 	@Column(name="nm_tipo_estabelecimento", nullable=false, length=100)
 	private String nome;
+	
+	@OneToMany(mappedBy="tipo")
+	private List<Estabelecimento> estabelecimentos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Estabelecimento> getEstabelecimentos() {
+		return estabelecimentos;
+	}
+
+	public void setEstabelecimentos(List<Estabelecimento> estabelecimentos) {
+		this.estabelecimentos = estabelecimentos;
+	}
+
+	public TipoEstabelecimento(int id, String nome, List<Estabelecimento> estabelecimentos) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estabelecimentos = estabelecimentos;
+	}
+
+	public TipoEstabelecimento() {
+		super();
+	}
+	
+	
 
 }
